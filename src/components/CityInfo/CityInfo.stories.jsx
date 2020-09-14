f480ce2fad4bab926d6529c99cac5e22d9389bea
NietakO <al316458@uji.es>
2020-09-14 21:41:04 +0200

    Initial commit

---
+++ src/components/CityInfo/CityInfo.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CityInfo from "./CityInfo";
+import "typeface-roboto"
+
+export default {
+    title: "CityInfo",
+    component : CityInfo
+}
+export const CityExample = () => <CityInfo city={"Villarreal"} country={"Spain"}/>
